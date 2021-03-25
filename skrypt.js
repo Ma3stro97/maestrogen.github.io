45b8519bbebe07769a1783e363c566fb64b39683
Ma3stro97 <kacprrwk@gmail.com>
2021-03-25 11:07:10 +0100

    Got it

---
+++ skrypt.js
@@ -0,0 +1,35 @@
+function xd() {
+    var a = [];
+    var b = [];
+    var c = [];
+    var d = [];
+    var e = [];
+    var f = [];
+    var g = [];
+    var h = [];
+    var i = [];
+    var j = [];
+    var k = [];
+    var l = [];
+    var m = [];
+    var n = [];
+    var o = [];
+a.push(Math.floor(Math.random() * 9) + 1);
+
+b.push(Math.floor(Math.random() * 9) + 1);
+
+c.push(Math.floor(Math.random() * 9) + 1);
+d.push(Math.floor(Math.random() * 9) + 1);
+    e.push(Math.floor(Math.random() * 9) + 1);
+    f.push(Math.floor(Math.random() * 9) + 1);
+    g.push(Math.floor(Math.random() * 9) + 1);
+    h.push(Math.floor(Math.random() * 9) + 1);
+    i.push(Math.floor(Math.random() * 9) + 1);
+    j.push(Math.floor(Math.random() * 9) + 1);
+    k.push(Math.floor(Math.random() * 9) + 1);
+    l.push(Math.floor(Math.random() * 9) + 1);
+    m.push(Math.floor(Math.random() * 9) + 1);
+    n.push(Math.floor(Math.random() * 9) + 1);
+    o.push(Math.floor(Math.random() * 9) + 1);
+    document.getElementById("wyn").innerHTML = "0" + a + b + c + "-" + d + e + f + g + "-" + h + i + j + k + "-" + l + m + n + o;
+}
